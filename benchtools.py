93d797593ed769d0522b719b8ebb13d789910710
Darix001 <100180688+Darix001@users.noreply.github.com>
2025-03-10 13:46:57 -0400

    Added benchtools

---
+++ benchtools.py
@@ -0,0 +1,18 @@
+from timeit import Timer
+from numbers import Integral
+from functools import partial
+from collections import Counter
+from collections.abc import Callable
+from more_itertools import repeatfunc
+
+fastest_timer = partial(min, key=Timer.timeit)
+
+
+def with_args(args, /, *funcs, iterations:Integral=100) -> dict[Callable, int]:
+	funcs = (partial(func, *args) for func in funcs)
+	return benchsort(*funcs, iterations=iterations)
+
+
+def benchsort(*funcs, iterations:Integral=100) -> dict[Callable, int]:
+	data = Counter(repeatfunc(fastest_timer, iterations, *map(Timer, funcs)))
+	return {tm.inner.__defaults__[0]:n for tm, n in data.most_common()}
